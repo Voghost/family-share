623b7fc17f414133145ee4ac3075ef137e8efa88
ligthblue <1242021176@qq.com>
2021-06-16 19:29:03 +0800

    备忘录样式

--- family_share/app/src/main/java/edu/dgut/network_engine/AddMemorandumActivity.kt
+++ family_share/app/src/main/java/edu/dgut/network_engine/AddMemorandumActivity.kt
@@ -1,29 +1,65 @@
 package edu.dgut.network_engine
 
+import android.icu.text.SimpleDateFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_add_memorandum.*
+import java.util.*
 
 class AddMemorandumActivity : AppCompatActivity() {
     private lateinit var save:Button
     private lateinit var reset:Button
-    private lateinit var edit:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_memorandum)
-        title="添加备忘录"
-        save=findViewById(R.id.btn_save)
-        reset=findViewById(R.id.btn_reset)
-        edit=findViewById(R.id.edit)
+        title = "添加备忘录"
+        save = findViewById(R.id.btn_save)
+        reset = findViewById(R.id.btn_reset)
         //清空
-        reset.setOnClickListener{
-            edit.setText("")
 
+        var memorandumMessage = edit.text.toString()
+        var content = getNow()
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            if ((month + 1) / 10 < 1) {
+                if (dayOfMonth / 10 < 1) {
+                    content = year.toString() + "-0" + (month + 1) + "-0" + dayOfMonth
+                } else {
+                    content = year.toString() + "-0" + (month + 1) + "-" + dayOfMonth
+                }
+            } else {
+                if (dayOfMonth / 10 < 1) {
+                    content = year.toString() + "-" + (month + 1) + "-0" + dayOfMonth
+                } else {
+                    content = year.toString() + "-" + (month + 1) + "-" + dayOfMonth
+                }
+            }
+
+            reset.setOnClickListener {
+                edit.setText("")
+
+            }
+            //保存
+            save.setOnClickListener {
+                Log.v("测试内容",memorandumMessage)
+                Log.v("测试时间",content)
+                finish()
+            }
         }
-        //保存
-        save.setOnClickListener{
-            finish()
+    }
+
+    fun getNow(): String {
+        if (android.os.Build.VERSION.SDK_INT >= 24) {
+            return SimpleDateFormat("yyyy-MM-dd").format(Date())
+        } else {
+            var tms = Calendar.getInstance()
+            return tms.get(Calendar.YEAR).toString() + "-" + tms.get(Calendar.MONTH)
+                .toString() + "-" + tms.get(Calendar.DAY_OF_MONTH).toString() + " " + tms.get(
+                Calendar.HOUR_OF_DAY
+            ).toString()
         }
+
     }
 }
