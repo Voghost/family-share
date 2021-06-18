d51e260826079a3ffb5f825514bcd1aabb6df9ac
liang-weishun <1123327216@qq.com>
2021-06-18 10:30:34 +0800

    删除备忘录日历

--- family_share/app/src/main/java/edu/dgut/network_engine/AddMemorandumActivity.kt
+++ family_share/app/src/main/java/edu/dgut/network_engine/AddMemorandumActivity.kt
@@ -19,48 +19,15 @@ class AddMemorandumActivity : AppCompatActivity() {
         save = findViewById(R.id.btn_save)
         reset = findViewById(R.id.btn_reset)
         //清空
-
-        var content = getNow()
-        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            if ((month + 1) / 10 < 1) {
-                if (dayOfMonth / 10 < 1) {
-                    content = year.toString() + "-0" + (month + 1) + "-0" + dayOfMonth
-                } else {
-                    content = year.toString() + "-0" + (month + 1) + "-" + dayOfMonth
-                }
-            } else {
-                if (dayOfMonth / 10 < 1) {
-                    content = year.toString() + "-" + (month + 1) + "-0" + dayOfMonth
-                } else {
-                    content = year.toString() + "-" + (month + 1) + "-" + dayOfMonth
-                }
-            }
-
-            var memorandumMessage = edit.text.toString()
-
-            reset.setOnClickListener {
-                edit.setText("")
-
-            }
-            //保存
-            save.setOnClickListener {
-                Log.v("测试内容",memorandumMessage)
-                Log.v("测试时间",content)
-                finish()
-            }
+        var memorandumMessage = edit.text.toString()
+        reset.setOnClickListener {
+            edit.setText("")
+        }
+        //保存
+        save.setOnClickListener {
+            Log.v("测试内容",memorandumMessage)
+            finish()
         }
     }
 
-    fun getNow(): String {
-        if (android.os.Build.VERSION.SDK_INT >= 24) {
-            return SimpleDateFormat("yyyy-MM-dd").format(Date())
-        } else {
-            var tms = Calendar.getInstance()
-            return tms.get(Calendar.YEAR).toString() + "-" + tms.get(Calendar.MONTH)
-                .toString() + "-" + tms.get(Calendar.DAY_OF_MONTH).toString() + " " + tms.get(
-                Calendar.HOUR_OF_DAY
-            ).toString()
-        }
-
-    }
 }
